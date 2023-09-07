62fc117ce0eee932674db193b4c5ac2d3a9317a0
Matman06 <63415111+Matman06@users.noreply.github.com>
2023-09-07 15:02:58 +0200

    Merge branch 'main' of https://github.com/ArnaudMasseron/projet-hackathon-ponts into main

--- src/utils/ask_question_to_pdf.py
+++ src/utils/ask_question_to_pdf.py
@@ -122,6 +122,7 @@ def ask_question_to_pdf(question):
     contexte = [{"role": "system", "content": preprompt}]
 
 nombre_questions = 2
+
 def ask_qcm():
     ReponseString = "[" + gpt3_completion(
         'Génère un qcm de ' + str(nombre_questions) + ' questions avec 1 réponse juste et 3 réponses fausses à partir du contexte fourni. Je veux que tu renvoies le qcm sous la forme suivante : {"answer": "Quelle est la capitale de la France ?","choices": ["Berlin", "Madrid", "Lisbonne", "Paris"],"correct": 4} Tu renvoies juste la réponse sous cette forme, tu ne renvoies rien d autre. Tu sépares les résultats par des virgules',
